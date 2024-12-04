3fffa3df61420b146ab9c50d20dc1b9fb7b50c4c
jhlubucek <jan.hlubucek@principal.cz>
2024-12-04 16:01:14 +0100

    day5 prep

--- AoC2024/src/main/java/org/example/Main.java
+++ AoC2024/src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
 
-        Day day = new Day4();
-        day.runTask(2);
+        Day day = new Day5();
+        day.runTask(1);
         long finish = System.currentTimeMillis();
         System.out.println("run time: " + (finish - start) + "ms");
     }
---
+++ AoC2024/src/main/java/org/example/days/Day5.java
@@ -0,0 +1,13 @@
+package org.example.days;
+
+public class Day5 extends Day{
+    @Override
+    public void task1() {
+
+    }
+
+    @Override
+    public void task2() {
+
+    }
+}
