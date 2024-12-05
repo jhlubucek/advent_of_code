0ac3ca1eea7db05572f416e5b090461f173cc439
jhlubucek <jan.hlubucek@principal.cz>
2024-12-05 23:24:36 +0100

    day6 prep

--- AoC2024/src/main/java/org/example/days/Day6.java
+++ AoC2024/src/main/java/org/example/days/Day6.java
@@ -1,19 +1,16 @@
 package org.example.days;
 
-import org.example.common.Helpers;
-import org.example.dataReaders.DataReader;
 import org.example.dataReaders.LineDataReader;
 
 import java.util.List;
 
-import static org.example.common.Helpers.*;
 import static org.example.dataReaders.DataReader.FileType.INPUT;
 import static org.example.dataReaders.DataReader.FileType.TEST;
 
-public class DayTemplate extends Day{
+public class Day6 extends Day{
     @Override
     public void task1() {
-        List<String> input = LineDataReader.getLines("day", TEST,false);
+        List<String> input = LineDataReader.getLines("day6", TEST,false);
 
     }
 
